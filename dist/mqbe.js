4e8ed612226baa7932720f617948af8380b854a0
Victor Ortiz <kespers@gmail.com>
2015-11-29 19:59:49 +0100

    Define and trigger jquery events

--- dist/mqbe.js
+++ dist/mqbe.js
@@ -11,111 +11,53 @@
  * Released on: November 29, 2015
  */
 
-var MQBE = {
+(function($, undefined) {
+  'use strict';
 
-  v: '1.04',
-
-  data: {
-    mq_supported: false,
-    current_state: null,
-    previous_state: null
-  },
-
-  init: function() {
-    this.detect_mq_support();
-    this.data.current_state = this.get_current_state();
-    this.data.previous_state = this.data.current_state;
-  },
-
-  // Event queue
-  events: {
-    enter: {},
-    leave: {}
-  },
+  var $doc = $(document),
+      current_state;
 
   // If state changed sets the data vars and tries to launch the callback (if exists)
-  check_state: function () {
-    this.data.current_state = this.get_current_state();
-    if ( this.data.previous_state !== this.data.current_state) {
-      // If there are on-leave actions fire them
-      if ( typeof this.events.leave[this.data.previous_state] !== 'undefined' && this.events.leave[this.data.previous_state] !== null && this.events.leave[this.data.previous_state].length > 0) {
-        // Launch all events in queue
-        for (i = 0, len = this.events.leave[this.data.previous_state].length; i < len; i++) {
-          this.events.leave[this.data.previous_state][i]();
-        }
-      }
-      if (typeof this.events.enter[this.data.current_state] !== 'undefined' && this.events.enter[this.data.current_state].length > 0) {
-        // Launch all events in queue
-        for (i = 0, len = this.events.enter[this.data.current_state].length; i < len; i++) {
-          this.events.enter[this.data.current_state][i]();
-        }
-      }
-      this.data.previous_state = this.data.current_state;
-    }
-  },
+  function check_state() {
+    var previous_state = current_state;
+    current_state = get_current_state();
 
-  get_current_state: function () {
-    var state = window.getComputedStyle(document.body,':after').getPropertyValue('content');
-    state = state.replace( /["']/g,''); // Firefox / Chrome 43 bugfix
-    return state;
-  },
-
-  // Has the browser mediaqueries support?
-  detect_mq_support: function() {
-    if(typeof window.matchMedia !== 'undefined' || typeof window.msMatchMedia !== 'undefined') {
-      this.data.mq_supported = true;
+    if ( previous_state !== current_state) {
+      $doc.trigger('mqbe.leave.' + previous_state);
+      $doc.trigger('mqbe.enter.' + current_state);
     }
-    return this.data.mq_supported;
-  },
-
-  // Add evento to the queue!
-  on: function( action, state, code) {
-    if (typeof this.events[action][state] === 'undefined') {
-      this.events[action][state] = [];
-    }
-    // If event added in current state and "enter"
-    if (state === this.data.current_state && action === 'enter') {
-      code();
-    }
-    this.events[action][state].push(code);
-    return this;
-  },
-
-  // Clear event for state evento to the queue!
-  off: function( action, state) {
-    if (typeof this.events[action][state] !== 'undefined') {
-      this.events[action][state].length = 0;
-    }
-    return this;
+    return current_state;
   }
 
-}; //MQBE
+  // Get state from body:after content
+  function get_current_state() {
+    var state = window.getComputedStyle(document.body,':after').getPropertyValue('content');
+    return state.replace( /["']/g,''); // Firefox / Chrome 43 bugfix
+  }
 
-// Self init IE9+ $.documentready
-if (document.addEventListener) {
-  document.addEventListener('DOMContentLoaded', function(event) {
-    // La magia aquí!
-    MQBE.init();
+  // Has the browser mediaqueries support?
+  function detect_mq_support() {
+    return (typeof window.matchMedia !== 'undefined' || typeof window.msMatchMedia !== 'undefined');
+  }
 
-    // Start listener ASAP if mq are supported
-    if (MQBE.data.mq_supported) {
-      debounced_resize(function() {
-        MQBE.check_state();
+  // Debounced resize
+  // https://github.com/louisremi/jquery-smartresize/
+  function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,250);};return c;}
+
+  if (detect_mq_support()) {
+    $(function() {
+      check_state();
+
+      // Start listener ASAP if mq are supported
+      on_resize(function() {
+        check_state();
       });
-    }
-  });
-}
+    });
+  }
 
-// Debounced resize
-// https://github.com/louisremi/jquery-smartresize/
-function debounced_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,100);};return c;}
+  window.MQBE = {
+    supported: detect_mq_support,
+    current_state: get_current_state
+  };
 
-/*
-MQBE.on('enter', 'tablet', function() {
-  console.log('Enter mediaquery: '+this.data.current_state);
-});
-
-MQBE.on('enter', 'tablet', function() {
-  console.log('Leave mediaquery: '+this.data.current_state);
-});
-*/
+})(jQuery);
