bc9cf5689754f93d4b166177557c5654892f5a93
Victor Ortiz <kespers@gmail.com>
2015-11-29 20:00:52 +0100

    Using document attached events, document.ready it’s not necessary

--- demo/ready.js
+++ demo/ready.js
@@ -1,51 +1,42 @@
-$(document).ready(function() {
-  // La magia aquí!
-
+(function() {
   var counter = 0;
 
   // //////////////////////////////////
   // MQ Responsive breakpoints
   // //////////////////////////////////
 
-  MQBE.off('enter', 'desktop').
-    on('enter', 'desktop', function() {
+  $(document)
+    .on('mqbe.enter.desktop', function() {
       $('body').prepend('<p>'+counter+'. Entered desktop</p>');
       counter++;
-    }).on('enter', 'desktop', function() {
-      console.log('Secondary event added to the same state!');
+    })
+    .on('mqbe.enter.small_desktop', function() {
+      $('body').prepend('<p>'+counter+'. Entered small-desktop</p>');
+      counter++;
+    })
+    .on('mqbe.enter.tablet', function() {
+      $('body').prepend('<p>'+counter+'. Entered tablet</p>');
+      counter++;
+    })
+    .on('mqbe.enter.mobile', function() {
+      $('body').prepend('<p>'+counter+'. Entered mobile</p>');
+      counter++;
+    })
+    .on('mqbe.leave.desktop', function() {
+      $('body').prepend('<p>'+counter+'. --- > Leaved desktop</p>');
+      counter++;
+    })
+    .on('mqbe.leave.small_desktop', function() {
+      $('body').prepend('<p>'+counter+'. --- > Leaved small-desktop</p>');
+      counter++;
+    })
+    .on('mqbe.leave.tablet', function() {
+      $('body').prepend('<p>'+counter+'. --- > Leaved tablet</p>');
+      counter++;
+    })
+    .on('mqbe.leave.mobile', function() {
+      $('body').prepend('<p>'+counter+'. --- > Leaved mobile</p>');
+      counter++;
     });
 
-  MQBE.on('enter', 'small_desktop', function() {
-    $('body').prepend('<p>'+counter+'. Entered small-desktop</p>');
-    counter++;
-  }).on('leave', 'small_desktop', function() {
-    $('body').prepend('<p>'+counter+'. --- > Leaved small-desktop</p>');
-    counter++;
-  });
-
-  MQBE.on('enter', 'tablet', function() {
-    $('body').prepend('<p>'+counter+'. Entered tablet</p>');
-    counter++;
-  });
-
-  MQBE.on('enter', 'mobile', function() {
-    $('body').prepend('<p>'+counter+'. Entered mobile</p>');
-    counter++;
-  });
-
-  MQBE.on('leave', 'desktop', function() {
-    $('body').prepend('<p>'+counter+'. --- > Leaved desktop</p>');
-    counter++;
-  });
-
-  MQBE.on('leave', 'tablet', function() {
-    $('body').prepend('<p>'+counter+'. --- > Leaved tablet</p>');
-    counter++;
-  });
-
-  MQBE.on('leave', 'mobile', function() {
-    $('body').prepend('<p>'+counter+'. --- > Leaved mobile</p>');
-    counter++;
-  });
-
-});
+})();
